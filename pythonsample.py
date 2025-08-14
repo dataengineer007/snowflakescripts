c80c69abe207200bf27cf57988623f8cc813474c
dataengineer007 <Vishalkaushal2506@gmail.com>
2025-08-14 17:08:33 +0530

    Update pythonsample.py

--- pythonsample.py
+++ pythonsample.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
-today = datetime.now()
-date_str = today.strftime("%d%b%Y")
+def main(session):  # Snowflake passes a session object here
+    today = datetime.now()
+    date_str = today.strftime("%d%b%Y")
+    return f"Today is {date_str}"
 
-#print result
-print(f"Today is {date_str}")
