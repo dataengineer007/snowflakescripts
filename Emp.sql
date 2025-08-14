25901dafce0dd28a47c705c446606f678a62c824
dataengineer007 <Vishalkaushal2506@gmail.com>
2025-08-14 17:41:36 +0530

    Update Emp.sql updating table name

--- Emp.sql
+++ Emp.sql
@@ -1,7 +1,8 @@
-CREATE TABLE employees (
+CREATE TABLE EMP_TABLE (
     employee_id INT PRIMARY KEY,
     first_name VARCHAR(50),
     last_name VARCHAR(50),
     department VARCHAR(50),
     salary DECIMAL(10, 2)
 );
+
